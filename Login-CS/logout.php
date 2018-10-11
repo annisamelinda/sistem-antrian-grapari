03c105607ffe8aaf9751550f10a7d28397685b1b
irwanhub2016 <irwansyarifudin16@gmail.com>
2018-10-11 12:39:36 +0700

    update logout.php

--- Login-CS/logout.php
+++ Login-CS/logout.php
@@ -18,7 +18,7 @@ session_start();
 
 	include 'db.php';
 	$session_NIK = $_SESSION['NIK'];
-
+	$nul = 'NULL';
 	$sql_view_loket="SELECT pl.NIK, nama, no_loket, access_time, status_loket FROM petugas pl JOIN pelayanan_loket pn ON pl.NIK = pn.NIK where pl.NIK ='$session_NIK'";
 
 	$result=mysqli_query($con,$sql_view_loket);
@@ -29,16 +29,16 @@ session_start();
 
 	$tag_loket = $row["no_loket"];
 
-	$null_loket="UPDATE pelayanan_loket SET status_loket = 0 WHERE no_loket = '$tag_loket' AND NIK = '$session_NIK';";
+	$null_loket="UPDATE pelayanan_loket SET status_loket = 0, NIK = NULL WHERE no_loket = '$tag_loket' AND NIK = '$session_NIK';";
 
 	$to_nol=mysqli_query($con,$null_loket); 
 
 	if($to_nol)
 	{
-	// menghapus semua session
+	//menghapus semua session
 	session_destroy();
 	 
-	// mengalihkan halaman sambil mengirim pesan logout
+	 //mengalihkan halaman sambil mengirim pesan logout
 	header("location: index.php");
 	}
 
